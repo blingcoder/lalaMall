8bf00d7a0ed0398963d348bc39d7cbc7a1e06ae4
blingcoder <312124870@qq.com>
2018-10-05 23:30:59 +0800

    details

--- lala.sql
+++ lala.sql
@@ -58,15 +58,16 @@ CREATE TABLE la_floor_column(
 	column_title VARCHAR(100), #列标题
 	column_subtitle VARCHAR(100), #列副标题
 	column_price VARCHAR(100), #列价格
-	column_ad_img VARCHAR(100) #列图片
+	column_ad_img TEXT #列图片	
 )
 
 #商品
 CREATE TABLE la_product(
-	pid INT PRIMARY KEY AUTO_INCREMENT,
+	sku INT PRIMARY KEY AUTO_INCREMENT,
+	pid INT,
 	family_id INT,
 	title VARCHAR(100),
-	discount VARCHAR(100),
+	discount_msg VARCHAR(100),
 	onsale_price DECIMAL(10,2),
 	origin_price DECIMAL(10,2),
 	month_sold INT,
@@ -74,11 +75,19 @@ CREATE TABLE la_product(
 	total_comments_num INT,
 	taste VARCHAR(200),
 	package VARCHAR(200),
-	storage INT,
-	pro_param VARCHAR(1024)，
-	detail_pics VARCHAR(1024)
+	storage INT
 )
 INSERT INTO la_product VALUES(null,1,"良品铺子 手剥松子218g 坚果炒货 巴西松子","购物满2件打8折，满3件7折",56.9,98,1015,6015,640,"原味*奶油*炭烧*咸香","手袋单人份*礼盒双人份*全家福礼包",1000,"**********","*******")
+#商品详情
+CREATE TABLE la_product_details(
+	did INT PRIMARY KEY AUTO_INCREMENT;
+	pid INT,
+	pro_param TEXT，
+	detail_pics TEXT
+)
+INSERT INTO la_product_details(pid, pro_param, detail_pics) VALUES(1,"[{\"产品类型\":\"烘炒类\"},{\"原料产地\":\"巴基斯坦\"},{\"产地\":\"湖北省武汉市\"},{\"配料表\":\"进口松子、食用盐\"},{\"产品规格\":\"210g\"},{\"保质期\":\"180天\"},{\"产品标准号\":\"GB/T 22165\"},{\"生产许可证编号\":\"QS4201 1801 0226\"},{\"存储方法\":\"请放置于常温、阴凉、通风、干燥的环境下\"},{\"食用方法\":\"开袋去壳即食\"}]","[{\"url\":\"img/tw1.jpg\"},{\"url\":\"img/tw2.jpg\"},{\"url\":\"img/tw3.jpg\"},{\"url\":\"img/tw4.jpg\"},{\"url\":\"img/tw5.jpg\"},{\"url\":\"img/tw6.jpg\"},{\"url\":\"img/tw7.jpg\"}]")
+
+
 #商品评价
 CREATE TABLE la_product(
 	cid INT PRIMARY KEY AUTO_INCREMENT,
@@ -96,3 +105,111 @@ CREATE TABLE la_pro_pic(
 	md VARCHAR(200),
 	lg VARCHAR(200)
 )
+
+
+
+
+#楼层
+{
+    "id":100111,
+    "type":2,
+    "title":"甜品",
+    "sub_title":"每一道甜品都有一个故事",
+    "links":[
+        {
+            "name":"桂花糕",
+            "url":"http://www.baidu.com/abc.jpg"
+        },
+        {
+            "name":"奶皮酥",
+            "url":"http://www.baidu.com/abc.jpg"
+        },
+        {
+            "name":"栗子糕",
+            "url":"http://www.baidu.com/abc.jpg"
+        },
+        {
+            "name":"马卡龙",
+            "url":"http://www.baidu.com/abc.jpg"
+        },
+        {
+            "name":"铜锣烧",
+            "url":"http://www.baidu.com/abc.jpg"
+        },
+        {
+            "name":"豌豆黄",
+            "url":"http://www.baidu.com/abc.jpg"
+        }
+    ],
+    "link_seperator":"|",
+    "col1":{
+        "links":[
+            {
+                "name":"核桃",
+                "url":"http://www.baidu.com/abc.jpg"
+            },
+            {
+                "name":"核桃",
+                "url":"http://www.baidu.com/abc.jpg"
+            },
+            {
+                "name":"核桃",
+                "url":"http://www.baidu.com/abc.jpg"
+            },
+            {
+                "name":"核桃",
+                "url":"http://www.baidu.com/abc.jpg"
+            },
+            {
+                "name":"核桃",
+                "url":"http://www.baidu.com/abc.jpg"
+            },
+            {
+                "name":"核桃",
+                "url":"http://www.baidu.com/abc.jpg"
+            }
+        ],
+        "title":"零食大礼包开抢啦",
+        "sub_title":"当小鱼儿恋上软豆腐",
+        "url":"http://www.baidu.com/1231313.jpg",
+        "index_img":"http://www.baidu.com/1231313.jpg"
+    },
+    "col2":[
+        {
+            "title":"雪之恋和风大福",
+            "price":13.8,
+            "url":"http://www.baidu.com/abc.jpg"
+        }
+    ],
+    "col3":[
+        {
+            "title":"小悠布丁",
+            "price":4.8,
+            "url":"http://www.baidu.com/1231312313.jpg"
+        },
+        {
+            "title":"小悠布丁",
+            "price":4.8,
+            "url":"http://www.baidu.com/1231312313.jpg"
+        }
+    ],
+    "col4":[
+        {
+            "title":"小悠布丁",
+            "price":4.8,
+            "url":"http://www.baidu.com/1231312313.jpg"
+        }
+    ],
+    "col5":[
+        {
+            "title":"小悠布丁",
+            "price":4.8,
+            "url":"http://www.baidu.com/1231312313.jpg"
+        },
+        {
+            "title":"小悠布丁",
+            "price":4.8,
+            "url":"http://www.baidu.com/1231312313.jpg"
+        }
+    ]
+}
--- static/js/pro_details.js
+++ static/js/pro_details.js
@@ -31,16 +31,131 @@ $(function(){
     var arrowL = $("#arrowL"),
         arrowR = $("#arrowR");
     var imgs = arrowL.next().children();
-    var count = 0,
+    var moved = 0,
         minWidth = 70;
     arrowR.click(function(){
-        count++;
-        imgs.css({
-            "left": -count*minWidth,
-            "transition": "all linear .3s"
-        });
+        if(!arrowR.is(".disabled")){
+            moved++;
+            imgs.css({
+                "left": -moved*minWidth,
+                "transition": "all linear .3s"
+            });
+            arrowL.removeClass("disabled");
+            if(imgs.children().length-5==moved){
+                arrowR.addClass("disabled");
+            }
+        }
+    })
+    arrowL.click(function(){
+        if(!arrowL.is(".disabled")){
+            moved--;
+            imgs.css({
+                "left": -moved*minWidth,
+                "transition": "all linear .3s"
+            });
+            arrowR.removeClass("disabled");
+            if(moved==0){
+                arrowL.addClass("disabled");
+            }
+        }
+    })
+    var md_div = $("#md_div");
+    // 鼠标移入小图，中图和大图切换到对应的
+    imgs.mouseover(function(e){
+        var $img = $(e.target);
+        if(e.target.nodeName=="IMG")
+        var str = $img.prop("src");
+        var num = parseInt(str.slice(-11));
+        md_div.children().prop("src",`img/0${num}_mid.jpg`);
+        // lg_div.css("backgroundImage",`url("file:///D:/12vsc_workspace/lalaMall/static/img/0${num}.jpg")`);
     })
 
-    $("#details_item").click();
+    // 口味、包装选择
+    var taste = $("#taste"),
+        pack = $("#pack");
+    function select(e){
+        var $sel = $(e.target);
+        if($sel.is("li")){
+            $sel.siblings().removeClass("active");
+            $sel.addClass("active");
+        }
+    }
+    taste.on("click",select);
+    pack.on("click",select);
+    // 数量点击增减
+    var numb = $("#num"),
+        minu = numb.children("span:eq(0)"),
+        selNum = numb.children("input"),
+        val = selNum.val(),
+        add = numb.children("span:eq(1)"),
+        store = numb.children("span:last").children(),
+        leftC = parseInt(store.html());
+        minu.click(function(){
+            if(selNum.val()<=1){
+                return;
+            }else{
+                val--;
+                selNum.val(val);
+                leftC++;
+                store.html(leftC);
+            }
+        })
+        add.click(function(){
+            val++;
+            selNum.val(val);
+            leftC--;
+            store.html(leftC);
+        })
+
+    
+    var details_title = $("#details_title");
+    // 宝贝详情、全部评价、猜你喜欢点击切换
+    details_title.click(function(e){
+        e.preventDefault();
+        var top = $("#details_right").offset().top;
+        $(document).scrollTop(top);
+        $title = $(e.target);
+        if($title.is("a")){
+            $title.parent().siblings().children().removeClass("active");
+            $title.addClass("active");
+            var id = $title.attr("data-target"),
+                intro = $("#intro"),
+                comments = $("#comments"),
+                prefer = $("#prefer");
+            if(id!="#comments"){
+                comments.removeClass("hideL").removeClass("hideR");
+                if(id=="#intro"){
+                    comments.addClass("hideR");
+                    prefer.addClass("hide");
+                }else if(id=="#prefer"){
+                    comments.addClass("hideL");
+                    intro.addClass("hide");
+                }
+                $(id).siblings().removeClass("show");
+                $(id).removeClass("hide").addClass("show");
+            }else{
+                $(id).siblings().removeClass("show").addClass("hide");
+                if($(id).hasClass("hideR")){
+                    $(id).removeClass("hideR").addClass("show");
+                }else{
+                    $(id).removeClass("hideL").addClass("show");
+                }
+            }
+        }
+    });
+    
+    // 固定宝贝详情title的位置
+    $(window).scroll(function(){
+        var toTop = details_title.offset().top-$(document).scrollTop();
+        if(toTop<=0){
+            details_title.addClass("fixed");
+            details_title.css("left",details_title.offset().left);
+        }
+        if($("#details_content").offset().top-$(document).scrollTop()>=0){
+            details_title.removeClass("fixed");
+        }
+    })
+    
+    
 
 })
